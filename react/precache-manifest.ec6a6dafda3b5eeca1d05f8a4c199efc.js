9aff3efe1ff1c364a33a07faf6eb7bf3af68413f
Aleksi Kaistinen <aleksi.kaistinen@allu.io>
2020-10-14 16:15:18 +0300

    card stg

---
+++ react/precache-manifest.ec6a6dafda3b5eeca1d05f8a4c199efc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8834e1e88a45a85960311b7bec6c6a34",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.efc68f11ad94beec2e92.bundle.js"
+  },
+  {
+    "url": "runtime~main.efc68f11ad94beec2e92.bundle.js"
+  },
+  {
+    "revision": "f46b05f66d0d6a1f5b3c",
+    "url": "static/css/main.b58d8be4.chunk.css"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "url": "vendors~main.efc68f11ad94beec2e92.bundle.js"
+  },
+  {
+    "url": "vendors~main.efc68f11ad94beec2e92.bundle.js.LICENSE.txt"
+  }
+]);
